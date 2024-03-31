e7a7202abe9d070da386d2a16b841e8444a48842
Muhammad Adil Mehmood <iamadilmalick@gmail.com>
2024-03-31 14:18:05 +0500

    first commit

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var leftDiceImage: UIImageView!
+    @IBOutlet weak var rightDiceImage: UIImageView!
+    
+    let allDiceImages = [UIImage(imageLiteralResourceName: "DiceOne"), UIImage(imageLiteralResourceName: "DiceTwo"), UIImage(imageLiteralResourceName: "DiceThree"), UIImage(imageLiteralResourceName: "DiceFour"), UIImage(imageLiteralResourceName: "DiceFive"), UIImage(imageLiteralResourceName: "DiceSix"),
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        }
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        leftDiceImage.image = allDiceImages[Int.random(in: 0...5)]
+        rightDiceImage.image = allDiceImages[Int.random(in: 0...5)]
+        
     }
-
-
+    
 }
 
